3792638f57a93d3915f04b9a74aabc48bc6af2c5
Alexandra Wheeler <awheeler2@wpi.edu>
2020-11-10 15:27:59 -0500

    Update game.js

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -1,20 +1,58 @@
+import { Scene } from 'phaser';
+import Ship from './ship';
+import Keyboard from './keyboard';
 
-import { Game } from 'phaser';
-import AsteroidScene from './scene';
+export default class SnakeScene extends Scene {
 
-const gameConfig = {
-  type: Phaser.WEBGL,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      fps: 60,
-      gravity: { y: 0 }
+  /**
+   * Use this function to load images which can be used in sprites
+   */
+  preload() {
+    // loads images which can be used in sprites
+    this.load.image('space', 'assets/media/images/deep-space.jpg');
+    this.load.image('bullet', 'assets/media/images/bullets.png');
+    this.load.image('ship', 'assets/media/images/ship.png');
+  }
+
+  /**
+   * Create game objects and stuff here
+   */
+  create() {
+    this.add.tileSprite(0, 0, 1600, 1200, 'space');
+    this.shipSpeedLabel = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });
+    this.ship = new Ship(this, 400, 300);
+    this.keyboard = new Keyboard(this);
+  }
+
+  /**
+   * This is where all the game logic goes. This is similar to the
+   * autonomousPeriodic and teleopPeriodic functions in robot code
+   */
+  update(time, delta) {
+    let acc = 200;
+    let turn = 180;
+    let forw = 0;
+    let turnw = 0;
+    if (this.keyboard.isUpPressed()){
+        forw += acc;
     }
-  },
-  scene: [new AsteroidScene()]
-};
+    else if (this.keyboard.isDownPressed()){
+        forw -= acc;
+    }
+    
+    if (this.keyboard.isLeftPressed()){
+        turnw -= turn;
+    }
+    if (this.keyboard.isRightPressed()){
+        turnw += turn;
+    }
 
-new Game(gameConfig);
+    this.ship.setForwardAcceleration(forw);
+    this.ship.setAngularVelocity(turnw);
+    this.ship.wrap();
+    this.ship.setDrag(true, .9775);
+   
 
+  }
+
+}
